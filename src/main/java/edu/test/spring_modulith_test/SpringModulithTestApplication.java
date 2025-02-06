687bd13d38606475737527d65e7081e6d84b3331
Diyan Yordanov <dgyordanov@gmail.com>
2025-02-06 15:36:36 +0100

    Setup a maven project

---
+++ src/main/java/edu/test/spring_modulith_test/SpringModulithTestApplication.java
@@ -0,0 +1,13 @@
+package edu.test.spring_modulith_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/test/spring_modulith_test/SpringModulithTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.test.spring_modulith_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
